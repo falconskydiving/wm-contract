026ac56a6b5936283c3edc61176d1c1c5a094cd8
falconskydiving <falcon.skydiving.dev@gmail.com>
2022-03-04 02:41:43 -0800

    Create NFT

--- contracts/v1_0/nft/NFT_V1_0.sol
+++ contracts/v1_0/nft/NFT_V1_0.sol
@@ -2,6 +2,25 @@
 
 pragma solidity ^0.8.0;
 
-contract NFT_V1_0 {
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract NFT_V1_0 is ERC1155, Ownable {
+  uint256 public constant MASTER = 0;
+  uint256 public constant GRAND_MASTER = 1;
+
+  constructor() ERC1155("") {
+    _mint(msg.sender, MASTER, 1089, "");
+    _mint(msg.sender, GRAND_MASTER, 33, "");
+  }
+
+  function mint(address account, uint256 id, uint256 amount) public onlyOwner {
+    _mint(account, id, amount, "");
+  }
+
+  function burn(address account, uint256 id, uint256 amount) public {
+    require(msg.sender == account);
+    
+    _burn(account, id, amount);
+  }
 }
