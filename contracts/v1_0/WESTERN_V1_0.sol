954afc391907013993bfa71ceef2334f3ddddfe6
falconskydiving <falcon.skydiving.dev@gmail.com>
2022-03-04 03:06:51 -0800

    Remove import line

--- contracts/v1_0/WESTERN_V1_0.sol
+++ contracts/v1_0/WESTERN_V1_0.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./RewardRun_V1_0.sol";
-
 contract WESTERN_V1_0 is ERC20, Ownable {
   constructor(address owner, uint256 tokenCount) ERC20("WESTERN MONEY", "WESTERN") {
     super._mint(owner, tokenCount * 10**18);
